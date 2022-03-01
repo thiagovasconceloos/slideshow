1eb2433d13fbb3fd0463df081f62d63dd291e23d
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-03-01 15:10:33 -0300

    Finalizando botões anterior e proximo

--- slideShow.js
+++ slideShow.js
@@ -3,13 +3,13 @@
 
 
 const images= [
-  
-      {'id' : ' 1', 'url' : '.img/jujutsu.jpg'},
-      {'id' : ' 2', 'url' : '.img/kimetsu.jpg'},
-      {'id' : ' 3', 'url' : '.img/fairytail.jpg'},
-      {'id' : ' 4', 'url' : '.img/bleach.jpg'},
-      {'id' : ' 5', 'url' : '.img/onepiece.jpg'},
-      {'id' : ' 6', 'url' : '.img/naruto.jpg'},
+// const images contem o endereço de cada imagem atraves do conceito "chave-valor", esta no formato json
+      {'id' : ' 1', 'url' : './img/jujutsu.jpg'},
+      {'id' : ' 2', 'url' : './img/kimetsu.jpg'},
+      {'id' : ' 3', 'url' : './img/fairytail.jpg'},
+      {'id' : ' 4', 'url' : './img/bleach.jpg'},
+      {'id' : ' 5', 'url' : './img/onepiece.jpg'},
+      {'id' : ' 6', 'url' : './img/naruto.jpg'},
 
 
 
@@ -17,9 +17,33 @@ const images= [
 
 ]
 
+// onde as imagens estão aparecendo.
+const containerItems  = document.querySelector('#container-items');
+
+
+const loadImages = (images,container) => {
+  
+
+    images.forEach(image => {
+          
+    container.innerHTML  += `
+        <div class='item'>
+        <img src='${image.url}'
+        </div>
+
+
+
+    
+    `
+
+
+    });
 
 
 
 
+}
 
-loadImages(images);
+
+
+loadImages(images,containerItems);
