bb89f925b5debc77870028035e5dd57872e410ff
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-03-01 14:36:22 -0300

    SlideShow em JS

---
+++ slideShow.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+
+const images= [
+  
+      {'id' : ' 1', 'url' : '.img/jujutsu.jpg'},
+      {'id' : ' 2', 'url' : '.img/kimetsu.jpg'},
+      {'id' : ' 3', 'url' : '.img/fairytail.jpg'},
+      {'id' : ' 4', 'url' : '.img/bleach.jpg'},
+      {'id' : ' 5', 'url' : '.img/onepiece.jpg'},
+      {'id' : ' 6', 'url' : '.img/naruto.jpg'},
+
+
+
+
+
+]
+
+
+
+
+
+
+loadImages(images);
